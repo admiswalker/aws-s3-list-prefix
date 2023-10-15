7176c5e46bfd50ecc69c4c96924e4a5c8a1c9cb9
admiswalker <admiswalker@gmail.com>
2023-10-15 16:43:03 +0900

    wip

--- main.py
+++ main.py
@@ -28,12 +28,47 @@ def aws_s3_ls_prefix_r(s3, bucket, prefix):
     l_ret.sort()
     return l_ret
 
+def remain_bottom(l_prefix):
+    l = []
+    for i in range(len(l_prefix)-1):
+        if l_prefix[i] in l_prefix[i+1]:
+            continue
+        l.append(l_prefix[i])
+    if l_prefix[-2] not in l_prefix[-1]:
+        l.append(l_prefix[-1])
+    return l
+
+def print_l(l):
+    for i in range(len(l)):
+        print(l[i])
+    return
+
+def get_first_object_name(s3, bucket, prefix):
+    marker = ''
+    ret = s3.list_objects(Bucket=bucket, Prefix=prefix, Marker=marker, MaxKeys=1)
+    l = [content['Key'] for content in ret['Contents']]
+    return l
+
 def main():
     bucket='test-9999'
     prefix=''
     s3 = boto3.client('s3')
     l_prefix = aws_s3_ls_prefix_r(s3, bucket, prefix)
-    print(l_prefix)
+    
+    print_l(l_prefix)
+    print()
+    
+    l = remain_bottom(l_prefix)
+    print_l(l)
+    
+    print()
+    print()
+    #print(l[0])
+    #name = get_first_object_name(s3, bucket, l[0])
+    #print(name)
+    for i in range(len(l)):
+        name = get_first_object_name(s3, bucket, l[i])
+        print(name)
 
 
 main()
